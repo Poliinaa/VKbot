2feeab66a5e350998a23b3c76f4c7b41afb74665
Полина <muhina.polina@mail.ru>
2024-11-07 15:54:45 +0300

    Update Bot_vk.py

--- Bot_vk.py
+++ Bot_vk.py
@@ -3,9 +3,9 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 # Инициализация бота
-vk_session = vk_api.VkApi(token='YOUR_TOKEN_HERE')
+vk_session = vk_api.VkApi(token='VK_TOKEN')
 vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, 'GROUP_ID_HERE')
+longpoll = VkBotLongPoll(vk_session, 'GROUP_ID')
 
 # Функция для отправки сообщений с клавиатурой
 def send_message(user_id, message, keyboard=None):
